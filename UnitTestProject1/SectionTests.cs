84eedef7caf3e713656c7324b04c894bcce7d9f1
forest93 <forest93@163.com>
2016-12-08 13:18:03 +0800

    Add unit test project.

---
+++ UnitTestProject1/SectionTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WikiDiffSummary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SectionTests
+    {
+        [TestMethod]
+        public void ParseSectionsTest()
+        {
+            var sections = WikitextSection.ParseSections(WikitextCases.Simple1A);
+            foreach (var s in sections)
+            {
+                Trace.WriteLine(s.Path);
+                Trace.WriteLine(s.Content);
+                Trace.WriteLine("----");
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/WikitextCases.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Contains sample wikitext strings.
+    /// </summary>
+    internal static class WikitextCases
+    {
+        public const string Simple1A = @"Text text.
+
+== A ==
+TextA
+=== A1 ===
+TextA1
+=== A2 ===
+TextA2
+== B ==
+TextB
+";
+        public const string Simple1B = @"Text text.
+
+== A ==
+TextA
+=== A1 ===
+TextA1
+=== A2 ===
+Foo
+== B ==
+TextB
+";
+
+    }
+}
